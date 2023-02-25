5c38cf36d3065b6918bc45c411ac94b6ca1dd3f3
Andrew <ahernandez@codeforamerica.org>
2023-02-25 12:07:16 -0800

    updates to views for future work on image hosting

--- home/views.py
+++ home/views.py
@@ -11,6 +11,14 @@ Purpose:
     to pull from and display product information in the shop section of the page.
 
 Variables:
+    #TODO: tempImageWorkAround
+    #tempImageWorkAround: This is a variable containing the path, /static/home/ - combined with {{product.product_image}} 
+        when these two are combined they form a complete path reference to the product image which is being hosted statically,
+        we cannot keep this as a permanent solution and this needs to be resolved soon(tm) via hosting media images proper via 
+        Amazon S3 or some other image hosting site to properly reference product images via CDN
+
+    #products: a reference to the products database
+
 Notes:
 '''
 def index(request):
@@ -20,6 +28,6 @@ def index(request):
         'home/index.html', 
         {
             'products':products,
-            'tempImageWorkAround': "/static/home/" 
+            'tempImageWorkAround': "/static/home/" #TODO cannot keep this as a permanent solution, need to implement external hosting for database and admin uploaded images
         }
     )

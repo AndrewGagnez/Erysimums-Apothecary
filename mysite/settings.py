f69441ac175b89659cafd100d32552fb029dea5b
Andrew <ahernandez@codeforamerica.org>
2023-02-24 20:33:02 -0800

    included other method to get secret key

--- mysite/settings.py
+++ mysite/settings.py
@@ -17,6 +17,7 @@ import os
 import environ
 env = environ.Env()
 environ.Env.read_env(env_file='.env')
+os.getenv('SECRET_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent

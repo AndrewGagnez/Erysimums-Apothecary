298a4b0a18254e12ff3628e1b001746a3fa41fcf
Andrew <ahernandez@codeforamerica.org>
2023-02-24 20:13:39 -0800

    heroku in settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -133,3 +133,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 #for file uploads
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+
+# Configure Django App for Heroku.
+import django_on_heroku
+django_on_heroku.settings(locals())

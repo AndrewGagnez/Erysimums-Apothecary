c62e69eb382d4050e2aac2042ecb1cae2667de0e
Andrew <ahernandez@codeforamerica.org>
2023-03-02 14:53:13 -0800

    moved account functions from shop to home

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('signup', SignUp.as_view(), name='signup'),
+    path('login', Login.as_view(), name='login'),
+    path('logout', views.Logout, name='logout'),
 ]

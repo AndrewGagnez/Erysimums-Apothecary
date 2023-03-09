8862c258736233b52852dfee751fb157a020b07d
Andrew <ahernandez@codeforamerica.org>
2023-03-09 14:34:23 -0800

    deleted forms.py

--- shop/forms.py
+++
@@ -1,7 +0,0 @@
-from paypal.standard.forms import PayPalPaymentsForm
-from django import forms
-from django.utils.html import format_html
-
-class CustomPayPalPaymentsForm(PayPalPaymentsForm):
-    def get_html_submit_element(self):
-        return """<button type="submit">Continue on PayPal website</button>"""
